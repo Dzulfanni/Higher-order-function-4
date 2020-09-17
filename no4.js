54c003376512b0dfe925abe6c593ac87b51ae09b
Dzulfanni <belajarayok866@gmail.com>
2020-09-18 00:13:40 +0700

    first commit

---
+++ no4.js
@@ -0,0 +1,4 @@
+var kodeUnixRandom = [12,78,42,40,36,2,8,96,66,20,91,85,41,69,38,77,97,60,15,57]; 
+const newNilai = kodeUnixRandom.reduce((accumulator,currentValue)=>accumulator+currentValue)
+
+document.write(newNilai/kodeUnixRandom.length);
